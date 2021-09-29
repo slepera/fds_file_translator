ef1d0598c40c5079b78d313c2998c94077ef6087
albac <alba.cuevas@outlook.com>
2021-09-29 11:34:57 +0200

    add script for EOP

--- src/main/java/plt/fds/filetranslator/FileTranslatorApplication.java
+++ src/main/java/plt/fds/filetranslator/FileTranslatorApplication.java
@@ -9,6 +9,7 @@ FileTranslatorApplication {
 
 	public static void main(String[] args) {
 		SolarFileTranslator sft = new SolarFileTranslator("C:\\Users\\albac\\OneDrive\\Escritorio\\NEXT\\PLATINO FDS\\TASKS\\20.09.21\\Solar Flux\\INPUT\\AP,KPNOAA.txt");
+		EOPTranslator eop = new EOPTranslator("C:\\Users\\albac\\OneDrive\\Escritorio\\NEXT\\PLATINO FDS\\TASKS\\20.09.21\\Solar Flux\\INPUT\\AP,KPNOAA.txt");
 		SpringApplication.run(FileTranslatorApplication.class, args);
 	}
 
