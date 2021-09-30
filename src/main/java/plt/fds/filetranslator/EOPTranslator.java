48614d730e4b7533764f6c4c378c209a1fe77935
albac <alba.cuevas@outlook.com>
2021-09-30 12:10:33 +0200

    EOP script done use substring because whitespaces are not equal in every line

--- src/main/java/plt/fds/filetranslator/EOPTranslator.java
+++ src/main/java/plt/fds/filetranslator/EOPTranslator.java
@@ -7,19 +7,13 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class EOPTranslator {
-    private String Lines =  "// this data is taken from Mean Nominal Long term Schatten predicts\r\n"
-            + "\r\n" // = new line
-            + "// data is given as F10.7 flux and Ap\r\n"
-            + "\r\n"
-            + "JR File\r\n"
-            + "\r\n"
-            + "F10.7 Ap\r\n"
-            + "\r\n";
 
     private class OutputStructure {
-        String date;
-        String Ap;
-        String Flux;
+        String MJD_utc;
+        String flags;
+        String PM_x;
+        String PM_y;
+        String ut1_utc;
     }
 
     public EOPTranslator(String string) {
@@ -28,50 +22,19 @@ public class EOPTranslator {
 
         try {
 
-//			File inputFile = new File(string);
-//			File outputFile = new File ("C:\\Users\\albac\\OneDrive\\Escritorio\\NEXT\\PLATINO FDS\\TASKS\\20.09.21\\Solar Flux\\" + DateTime.now().toString("dd.MM.yyyy  HH-mm-ss") + ".txt");
-
             BufferedReader br = new BufferedReader(new FileReader(string));
             String line;
-            int j = 0;
-            boolean flux = false;
-            boolean b1;
             while ((line = br.readLine()) != null) {
-                b1 = Pattern.matches(".*FLUX.*", line);
-                if (b1) {
-                    j = 0;
-                    flux = true;
+                OutputStructure tmp1 = new OutputStructure();
+                tmp1.MJD_utc = line.substring(7,15);
+                tmp1.flags = line.substring(16,17);
+                tmp1.PM_x = line.substring(18,27);
+                tmp1.PM_y = line.substring(37,46);
+                tmp1.ut1_utc = line.substring(58,68);
+                osa.add(tmp1);
                 }
 
-                boolean b3 = Pattern.matches("[0-9]{2}[a-zA-Z]{3}[0-9]{2}.*", line);
-
-                if (b3) {
-                    String[] tmp = line.split(" ");
-                    int i = 0;
-                    while (i < tmp.length) {
-                        if (flux) {
-                            if (i%2 == 0) {
-                                i ++;
-                            } else {
-                                osa.get(j).Flux = tmp[i];
-                                i++;
-                                j++;
-                            }
-                        } else {
-                            if (i%2 == 0) {
-                                OutputStructure tmp1 = new OutputStructure();
-                                tmp1.date = tmp[i];
-                                osa.add(tmp1);
-                                i ++;
-                            } else {
-                                osa.get(j).Ap = tmp[i];
-                                i++;
-                                j++;
-                            }
-                        }
-                    }
-                }
-            }	br.close();
+            br.close();
 
         }  catch (IOException e) {
             e.printStackTrace();
@@ -83,22 +46,15 @@ public class EOPTranslator {
 
             String europeanDatePattern = "dd.MM.yyyy  HH-mm-ss";
             DateTimeFormatter europeanDateFormatter = DateTimeFormatter.ofPattern(europeanDatePattern);
-            System.out.println(europeanDateFormatter.format(LocalDateTime.now()));
-            rb = new BufferedWriter(new FileWriter("C:\\Users\\albac\\OneDrive\\Escritorio\\NEXT\\PLATINO FDS\\TASKS\\20.09.21\\Solar Flux\\" + europeanDateFormatter.format(LocalDateTime.now()) + ".txt"));
-            rb.write(Lines);
+            rb = new BufferedWriter(new FileWriter("./data/output/EOP/ut1utc.dat " + europeanDateFormatter.format(LocalDateTime.now()) + ".txt"));
             for (int i = 0; i<osa.size(); i++) {
-                String month,year,day;
-                day = osa.get(i).date.substring(0, 2);
-                month = osa.get(i).date.substring(2, 5);
-                year = osa.get(i).date.substring(5, 7);
-
-                String date_final = month.toUpperCase() + " " + day + " " + "20" + year;
-
-                rb.write("      " + date_final + "    " + osa.get(i).Flux + "    " + osa.get(i).Ap);
+            /* CAREFUL with the whitespaces, due to the positive/negative numbers, that CAN NOT BE REMOVED */
+                rb.write("      " + osa.get(i).MJD_utc + "     " + osa.get(i).flags + "         " + osa.get(i).PM_x + "      " + osa.get(i).PM_y + "       " + osa.get(i).ut1_utc);
                 rb.newLine();
-                rb.newLine();
+
                 rb.flush();
-            } rb.close();
+            }
+                rb.close();
 
         } catch (IOException e) {
             System.out.println("Issue writing the information into the file.");
--- src/main/java/plt/fds/filetranslator/FileTranslatorApplication.java
+++ src/main/java/plt/fds/filetranslator/FileTranslatorApplication.java
@@ -9,8 +9,8 @@ FileTranslatorApplication {
 
 	public static void main(String[] args) {
 		/* add here translator class initialization */
-		SolarFileTranslator sft = new SolarFileTranslator("./data/input/solar flux/AP,KPNOAA.txt");
-	//	EOPTranslator eopTranslator = new EOPTranslator("./data/input/AP,KPNOAA.txt");
+	//	SolarFileTranslator sft = new SolarFileTranslator("./data/input/solar flux/AP,KPNOAA.txt");
+		EOPTranslator eopTranslator = new EOPTranslator("./data/input/EOP/finals2000A.all.txt");
 		SpringApplication.run(FileTranslatorApplication.class, args);
 	}
 
