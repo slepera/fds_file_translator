11152878a42d806a5c4eba66a734f79af147bcc3
albac <alba.cuevas@outlook.com>
2021-10-26 14:51:19 +0200

    first draft of CreateTPFText

--- src/main/java/plt/fds/filetranslator/ManoeuvrePlanXMLGenerator.java
+++ src/main/java/plt/fds/filetranslator/ManoeuvrePlanXMLGenerator.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class ManoeuvrePlanXMLGenerator {
 
@@ -206,15 +207,36 @@ public class ManoeuvrePlanXMLGenerator {
     private static Element CreateTPFText(TPF tpf) {
         Element TPF = new Element("TPF");
         String h1, h2, h3, h4, h5;
-        //Building h1, h2, h3, h4, h5
-        h1 = tpf.tpfHeader.taskName;
-        h1.concat(tpf.tpfHeader.taskType);
 
+        h1 = tpf.tpfHeader.taskName + " " + tpf.tpfHeader.taskType + " "
+                + tpf.tpfHeader.parameterSetName +" " + tpf.tpfHeader.parameterValueSetName;
 
+        h2 = "           " + tpf.tpfHeader.destination + " " + tpf.tpfHeader.source + " "
+                + tpf.tpfHeader.nbRecords;
 
-        //...........................
+        h3 = tpf.tpfHeader.releaseTime + " " + tpf.tpfHeader.earliestReleaseTime + " "
+                + tpf.tpfHeader.latestReleaseTime;
 
-        TPF.setText(h1+"\n"+h2+"\n");
+        h4 = tpf.tpfHeader.executionTime + " " + tpf.tpfHeader.subScheduleID;
+
+        h5 = tpf.tpfHeader.tpfRemarks;
+
+        String tpf_Header = h1 + "\n" + h2 + "\n" + h3 + "\n" + h4 + "\n" + h5 + "\n";
+        TPF.setText(tpf_Header);
+
+        String tpf_Body;
+        for (int g = 0; g <tpf.tpfBody.tpfRecord.size(); g++) {
+            String paramName = tpf.tpfBody.tpfRecord.get(g).paramName + " ";
+            String paramValueType = tpf.tpfBody.tpfRecord.get(g).paramValueType + " ";
+            String paramValue = tpf.tpfBody.tpfRecord.get(g).paramValue + " ";
+            String paramValueUnit = tpf.tpfBody.tpfRecord.get(g).paramValueUnit + " ";
+            String paramValueRadix = tpf.tpfBody.tpfRecord.get(g).paramValueRadix + " ";
+            String paramRemarks = tpf.tpfBody.tpfRecord.get(g).paramRemarks + "\n";
+
+            tpf_Body = paramName + paramValueType + paramValue + paramValueUnit + paramValueRadix + paramRemarks;
+            TPF.addContent(tpf_Body);
+        }
+
         return TPF;
     }
 
