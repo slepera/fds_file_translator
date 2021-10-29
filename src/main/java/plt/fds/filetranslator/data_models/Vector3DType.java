686dbb0f396a084e4fa54ee9a3dae6abcb07de78
Salvatore Lepera <salvatore.lepera.lavoro@gmail.com>
2021-10-29 09:48:17 +0200

    getter and setter to Vector3DType.java

--- src/main/java/plt/fds/filetranslator/data_models/Vector3DType.java
+++ src/main/java/plt/fds/filetranslator/data_models/Vector3DType.java
@@ -2,6 +2,31 @@ package plt.fds.filetranslator.data_models;
 
 public class Vector3DType {
     double x;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
     double y;
     double z;
 }
